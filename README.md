3e6ca72240d1174161953c039710e9befd79bbaf
Giulio Gratta <giulio@giuliogratta.com>
2020-04-16 19:28:46 -0700

    Basic implementation of the API
    - api.php is the main API file
    - documentation.json is displayed as in-system docs
    - .htaccess hides an optional settings.json file for deployment
    - food.svg is the default image, if none is provided with the recipe
    - make all requests get CORS headers

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple PHP API to serve recipes
+
+- A single PHP file runs the API. (api.php)
+- Self documenting, on errors the doc file is displayed (documentation.json)
+- Optional settings file, to set production API_KEY (settings.json)
+- settings file is hidden on the server installation (.htaccess)
---
+++ api.php
@@ -0,0 +1,122 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function getFileJSON($file) {
+    if (file_exists($file)) {
+        $handle = fopen($file, "r") or die("Unable to open file!");
+        $raw = fread($handle, filesize($file));
+        fclose($handle);
+        $json = json_decode($raw, true);
+        return $json;
+    } else {
+        return null;
+    }
+}
+function printJSON($json) {
+    header("Access-Control-Allow-Origin: *");
+    $output = json_encode($json, JSON_PRETTY_PRINT);
+    if(isset($_GET['pretty'])) {
+        echo '<pre>'; print_r($output); echo '</pre>';
+    } else {
+        echo $output."\n\r";
+    }
+}
+
+$SETTINGS = getFileJSON('settings.json');
+if (!$SETTINGS) {
+  $SETTINGS = [
+      "API_KEY" => "DEFAULT_KEY"
+  ];
+}
+
+if(isset($_GET['key']) && $_GET['key'] == $SETTINGS["API_KEY"]) {
+    function getRecipePhoto($recipe_name) {
+        $photo_path = 'recipes/'.$recipe_name.'/photo.jpg';
+        if (file_exists($photo_path)) {
+            return $_SERVER['HTTP_HOST'].'/'.$photo_path;
+        } else {
+            return $_SERVER['HTTP_HOST'].'/recipes/food.svg';
+        }
+    }
+
+    function displayDocs($title, $message) {
+        $doc_json = getFileJSON('documentation.json');
+        $doc_json['title'] = $title;
+        $doc_json['message'] = $message;
+        printJSON($doc_json);
+    }
+
+    if(isset($_GET['request'])) {
+        switch ($_GET['request']) {
+            case 'help':
+                displayDocs('Recipe API', 'Version 0.1.0');
+                break;
+            case 'get_one': # Get all of one recipe.
+                if (isset($_GET['recipe'])) {
+                    $recipe_name = $_GET['recipe'];
+                    $recipe_file = 'recipes/'.$recipe_name.'/recipe.json';
+                    if (file_exists($recipe_file)) {
+                        $json = getFileJSON($recipe_file);
+                        if (!is_null($json)) {
+                            $photo_url = getRecipePhoto($recipe_name);
+                            $json['photo'] = $photo_url;
+                            printJSON($json);
+                        } else {
+                            displayDocs('Parse Error.', 'There was a problem getting the recipe from the data directory.');
+                        }
+                    } else {
+                        displayDocs('Not Found.', "The recipe you requested doesn't exist.");
+                    }
+                } else {
+                    displayDocs('Missing Query String', 'The recipe query string is required for this request type');
+                }
+                break;
+            case 'get_multiple': # Get multiple recipes.
+                $output_json = [];
+                $files = glob('recipes/*/recipe.json');
+                shuffle($files);
+                foreach($files as $index=>$file) {
+                    $recipe_name = explode('/', $file)[1];
+                    $json = getFileJSON($file);
+                    if (!is_null($json)) {
+                        $recipe_json = [];
+                        $recipe_json['recipe_id'] = $recipe_name;
+                        $recipe_json['recipe_name'] = $json['recipe_name'];
+                        $recipe_json['description'] = $json['description'];
+                        $recipe_json['tags'] = $json['tags'];
+                        $recipe_json['times'] = $json['times'];
+                        $recipe_json['yields'] = $json['yields'];
+                        $recipe_json['photo'] = getRecipePhoto($recipe_name);
+                        array_push($output_json, $recipe_json);
+                    }
+                }
+                printJSON($output_json);
+                break;
+            case 'get_all_tags': # Get all unique tags from all recipes.
+                $files = glob('recipes/*/recipe.json');
+                $output_tags = [];
+                foreach($files as $file) {
+                    $recipe_name = explode('/', $file)[1];
+                    $json = getFileJSON($file);
+                    if (!is_null($json)) {
+                        foreach ($json['tags'] as $tag) {
+                            if (!in_array($tag, $output_tags)) {
+                                array_push($output_tags, $tag);
+                            }
+                        }
+                    }
+                }
+                sort($output_tags);
+                printJSON($output_tags);
+                break;
+            default:
+                displayDocs('Malformed Request', 'You must pass a recognized request type');
+                break;
+        }
+    } else { displayDocs('Not Found.', 'There is something wrong with your request.'); }
+} else {
+  print "Invalid API Key. You must provide a valid API key.";
+}
+?>
